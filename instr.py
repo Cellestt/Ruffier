b6962d1d2407da4dccf9f884a55534164c71ad4d
Lorreine <lorreinepriacilla@gmail.com>
2025-05-22 20:01:46 +0700

    instr.py

---
+++ instr.py
@@ -0,0 +1,47 @@
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+txt_hello = 'Welcome to the Health Status Detection Program!'
+txt_next = 'Start'
+
+txt_instruction = (
+    "This application allows you to use the Ruffier test to make an initial diagnosis of your health.\n\n"
+    "The Ruffier test is a set of physical exercises designed to assess your cardiac performance during physical exertion.\n"
+    "1. The subject lies in the supine position for 5 minutes and their pulse is measured for 15 seconds.\n"
+    "2. Then, within 45 seconds, the subject performs 30 squats.\n"
+    "3. After the exercise, the subject lies down again and their pulse is measured for the first 15 seconds,\n"
+    "    and then for the last 15 seconds of the first minute of the recovery period.\n\n"
+    "Important: If you feel unwell during the test (dizziness, tinnitus, shortness of breath, etc.), stop the test and consult a physician."
+)
+
+txt_title = 'Health'
+txt_name = 'Enter your full name:'
+txt_hintname = 'Full name'
+txt_hintage = '0'
+
+txt_test1 = (
+    "Lie on your back and take your pulse for 15 seconds. Click the 'Start first test' button to start the timer.\n"
+    "Write down the result in the appropriate field."
+)
+txt_test2 = (
+    "Perform 30 squats in 45 seconds. To do this, click the 'Start doing squats' button to start the squat counter."
+)
+txt_test3 = (
+    "Lie on your back and take your pulse for the first 15 seconds of the minute, then for the last 15 seconds of the minute.\n"
+    "Press the 'Start final test' button to start the timer.\n"
+    "The seconds that should be measured are indicated in green and the minutes that should not be measured are indicated in black.\n"
+    "Write down the results in the appropriate fields."
+)
+
+txt_sendresults = 'Send the results'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = 'Start the first test'
+txt_starttest2 = 'Start doing squats'
+txt_starttest3 = 'Start the final test'
+txt_timer = ''
+txt_age = 'Full years:'
+txt_finalwin = 'Results'
+txt_index = 'Ruffier Index: '
+txt_workheart = 'Cardiac performance:'
